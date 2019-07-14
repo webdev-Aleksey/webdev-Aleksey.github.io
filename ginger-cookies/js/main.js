166dfb6b63be102411da2155affe5bca8ca1a5f1
webdev-Aleksey <51025944+webdev-Aleksey@users.noreply.github.com>
2019-07-14 03:21:42 +0300

    add folder

---
+++ ginger-cookies/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    // $('.slider').slick();
+    $('.single-item').slick({
+      dots:true,
+      slidesToShow: 1,
+      speed:600,
+      autoplay:false,
+      autoplaySpeed: 3000,
+      adaptiveHeight: true,
+      centerMode: false
+      
+  
+    });
+  
+  });
