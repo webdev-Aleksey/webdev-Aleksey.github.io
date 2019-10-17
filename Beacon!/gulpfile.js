7da8437cd3b4f2b130efba12dfe4d2fc79b87bcf
webdev-Aleksey <51025944+webdev-Aleksey@users.noreply.github.com>
2019-10-17 16:49:37 +0300

    header done

--- Beacon!/gulpfile.js
+++ Beacon!/gulpfile.js
@@ -5,28 +5,20 @@ const gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync').create();
 
-
 gulp.task('serve', ['sass'], function() {
-
     browserSync.init({
         server: "app/"
     });
-
     gulp.watch("app/sass/*.sass", ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });   
 
-
 gulp.task('sass', function() {
-    return gulp.src("app/sass/*.sass")
+    return gulp.src("app/sass/main.sass")
         .pipe(sass())
-        .pipe(autoprefixer({browsers: ['last 8 versions'], cascade: false}))
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
-
-
-
 
 gulp.task('concat-css', function () {
     return gulp.src('app/css/*.css')
@@ -34,13 +26,11 @@ gulp.task('concat-css', function () {
       .pipe(gulp.dest('dist/'));
 });
 
-
 gulp.task('minify-css', function () {
     return gulp.src('dist/css/*.css')
       .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(gulp.dest('dist/css'));
   });
-
 
 gulp.task('autoprefixer', function () {
     gulp.src('app/css/*.css')
@@ -50,16 +40,5 @@ gulp.task('autoprefixer', function () {
     }))
     .pipe(gulp.dest('app/css'))
 });
-
-
-
-// exports.autoprefixer = () => (
-//     gulp.src('app/css/*.css')
-//         .pipe(autoprefixer({
-//             cascade: false
-//         }))
-//         .pipe(gulp.dest('app/css'))
-// );
-
 
 gulp.task('default', ['serve']);
