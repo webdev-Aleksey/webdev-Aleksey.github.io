844bb0b4bca2ad3653278b351a1796547799ba2f
webdev-Aleksey <51025944+webdev-Aleksey@users.noreply.github.com>
2019-10-27 01:23:39 +0300

    Revert "fixed js"
    This reverts commit f6f4c981fb9bd7d0722f4488f71b8e3462c6d481.

--- Beacon/gulpfile.js
+++ Beacon/gulpfile.js
@@ -1,25 +1,22 @@
-let gulp = require('gulp'),
+const gulp = require('gulp'),
     sass = require('gulp-sass'),
     concatCss = require('gulp-concat-css'),
+    cleanCSS = require('gulp-clean-css'),
     autoprefixer = require('gulp-autoprefixer'),
     purgecss = require('gulp-purgecss'),
     browserSync = require('browser-sync').create();
 
-    
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
         server: "app/"
     });
     gulp.watch("app/sass/*.sass", ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
-    gulp.watch("app/js/*.js").on('change', browserSync.reload);
 });   
-
 
 gulp.task('sass', function() {
     return gulp.src("app/sass/main.sass")
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(autoprefixer())
+        .pipe(sass())
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
@@ -30,16 +27,20 @@ gulp.task('concat-css', function () {
       .pipe(gulp.dest('dist/'));
 });
 
+gulp.task('minify-css', function () {
+    return gulp.src('dist/css/*.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest('dist/css'));
+  });
 
 gulp.task('autoprefixer', function () {
     gulp.src('app/css/*.css')
     .pipe(autoprefixer({
-        overrideBrowserslist: ['last 8 versions'],
+        browsers: ['last 8 versions'],
         cascade: false
     }))
     .pipe(gulp.dest('app/css'))
 });
-
 
 gulp.task('purgecss', function() {
     return gulp
